27945180cee343fdd955e97e947116d258bf4177
Aadarsh Senthil <aadhisen2006@gmail.com>
2020-08-09 12:23:25 -0400

    added files

---
+++ contactform.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Contact Form</title>
+        <link rel="stylesheet" href="contact-style.css">
+        <link href="https://fonts.googleapis.com/css2?family=Sora:wght@500&display=swap" rel="stylesheet">
+   
+    </head>
+    <body>
+        <div class="title">
+            <p>Contact Us</p>
+        </div>
+            <div class="contact-form">
+                <div class="form-box">
+                <form class="contact-form" action="" method="post">
+                    <input type="text" name="name" class="form-control" placeholder="Your Name" required>
+                    <br>
+
+                    <input type="text" name="email" class="form-control" placeholder="Your Email" required>
+                    <br>
+
+                    <input type="text" name="subject" class="form-control" placeholder="Subject" required>
+                    <br>
+
+                    <textarea name="message" class="form-control" placeholder="Your Message" required></textarea>
+                    <br>
+
+                    <button type="submit" class="form-control submit" name="submit">SEND MESSAGE</button>
+
+                </form>
+            </div>
+            </div>
+    </body>
+</html>
